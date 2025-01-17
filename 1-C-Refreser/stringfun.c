9730151779cd3dfff7dfa134151717d801ef741e
ChickenHop <162831705+ChickenHop@users.noreply.github.com>
2025-01-17 16:24:13 -0500

    finished act 1

--- 1-C-Refreser/stringfun.c
+++ 1-C-Refreser/stringfun.c
@@ -16,8 +16,55 @@ int  count_words(char *, int, int);
 
 
 int setup_buff(char *buff, char *user_str, int len){
-    //TODO: #4:  Implement the setup buff as per the directions
-    return 0; //for now just so the code compiles. 
+    int i = 0;  //number of charaters in buffer
+    int j = 0;  //number of charater copied form the user_str
+    int space=0; // 0 means that there was no prevoius space 1 means that there was a prevoius space
+    char *temp = user_str; 
+    int user_len = 0;
+    while (*temp != '\0'){
+        user_len++;
+        temp++;
+    }
+    if (user_len > len){
+         printf("String supplied is too long.");
+        exit(-1);
+    }
+    while (*user_str != '\0') { 
+        if (*user_str == ' ') {
+            // checks to make sure that there was no space before.
+            if (!space) { 
+                *buff = ' ';
+                space = 1;
+                i++;
+                j++;
+                buff++;        
+            }
+        }
+        // checks to see if there is a tab or new line and then convert it into a space
+        else if (*user_str == '\t' || *user_str == '\n'){
+            *buff = ' ';
+            space = 1;
+            i++;
+            j++;
+            buff++;
+           
+        }
+        else {
+            *buff = *user_str;
+            space = 0;
+            i++;
+            j++;
+            buff++;
+           
+        }
+        user_str++;
+    }
+    while (i < len){
+        *buff= '.';
+        buff++;
+        i++;
+    }
+    return j;        
 }
 
 void print_buff(char *buff, int len){
@@ -34,8 +81,49 @@ void usage(char *exename){
 }
 
 int count_words(char *buff, int len, int str_len){
-    //YOU MUST IMPLEMENT
-    return 0;
+    int word_count=0;
+    for (int i = 0; i < len; i++){
+        if(*buff == ' '){
+            word_count++;
+        }
+        if (*buff == '.'){
+            word_count++;
+            return word_count;
+        }
+        buff++;
+    }
+    return word_count;
+}
+
+void count_chars(char *buff, int len, int str_len) {
+    int i;
+    int char_count = 0;
+    int word_count = 1;
+    int new_word = 1; // if new word equal 1 if not equals 0
+
+    for (i = 0; i < len; i++) {
+        // 
+        if (new_word) {
+            printf("\n"); // put here to fix bug where after the frist word line all the others were offset by 1 space
+            printf("%d. ", word_count);
+            new_word = 0;
+            char_count = 0;
+        }
+
+        if (*buff == ' ') {
+            printf(" (%d)", char_count);
+            word_count++;
+            new_word = 1;
+        } else if (*buff == '.') { 
+            printf(" (%d)", char_count);
+            break; 
+        }
+
+        char_count++;
+        putchar(*(buff));
+        buff++;
+    }
+    printf("\n");
 }
 
 //ADD OTHER HELPER FUNCTIONS HERE FOR OTHER REQUIRED PROGRAM OPTIONS
@@ -49,7 +137,8 @@ int main(int argc, char *argv[]){
     int  user_str_len;      //length of user supplied string
 
     //TODO:  #1. WHY IS THIS SAFE, aka what if arv[1] does not exist?
-    //      PLACE A COMMENT BLOCK HERE EXPLAINING
+    //      It is safe because we frist check if there is 2 strings when the commandline is used. The frist string is the actual command and the second the string so 2. 
+    //      Since this is checked first if there is not 2 or higher for argc aka only the command then the checking of argv[1] doesn't happen.
     if ((argc < 2) || (*argv[1] != '-')){
         usage(argv[0]);
         exit(1);
@@ -66,7 +155,8 @@ int main(int argc, char *argv[]){
     //WE NOW WILL HANDLE THE REQUIRED OPERATIONS
 
     //TODO:  #2 Document the purpose of the if statement below
-    //      PLACE A COMMENT BLOCK HERE EXPLAINING
+    //      The purpose of this if statement is to check if there are 4 strings inputed. Since the command only work with 2 or 3 there should not be a 4th used. 
+    //      So to make sure that only 2 or 3 are used we exit the command to avoid any uninted bugs if 4 strings are inputed.
     if (argc < 3){
         usage(argv[0]);
         exit(1);
@@ -77,8 +167,11 @@ int main(int argc, char *argv[]){
     //TODO:  #3 Allocate space for the buffer using malloc and
     //          handle error if malloc fails by exiting with a 
     //          return code of 99
-    // CODE GOES HERE FOR #3
-
+    buff = (char *)malloc(BUFFER_SZ * sizeof(char));
+    if (buff == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(99);
+    }
 
     user_str_len = setup_buff(buff, input_string, BUFFER_SZ);     //see todos
     if (user_str_len < 0){
@@ -95,9 +188,21 @@ int main(int argc, char *argv[]){
             }
             printf("Word Count: %d\n", rc);
             break;
+//TODO:  #5 Implement the other cases for 'r' and 'w' by extending
+        case 'r':
+            printf("Reversed string: ");
+            for (int i= user_str_len-1; i >=0; i--){
+                putchar(*(buff+i));
+            }
+            putchar('\n');
+            break;
+        case 'w':
+            printf("Word Print\n");
+            printf("----------");
+            count_chars(buff,BUFFER_SZ,user_str_len);
+            break;
+            
 
-        //TODO:  #5 Implement the other cases for 'r' and 'w' by extending
-        //       the case statement options
         default:
             usage(argv[0]);
             exit(1);
@@ -105,6 +210,7 @@ int main(int argc, char *argv[]){
 
     //TODO:  #6 Dont forget to free your buffer before exiting
     print_buff(buff,BUFFER_SZ);
+    free(buff);
     exit(0);
 }
 
